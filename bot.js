aefe10e72f182f1502d1677930122764f3191544
VinzPropler <109711698+VinzPropler@users.noreply.github.com>
2022-09-04 11:37:36 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -283,4 +283,4 @@ client.on('error', e => {
   console.log(chalk.b.red(e.replace(regToken, 'that was.redacted')));
 });
 
-client.login("PASTE YOUR BOT TOKEN");
+client.login("MTAxNTg0MTI2MzIxMzM2NzM0OA.GhAk1o.KAy9-p9cciammTLd3MOP9RcLdPZY1tWdgXq__U");
